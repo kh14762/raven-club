f6174d742990811ede8aa71796a989e3cb3b1e41
kev <kevin.j.heritage@gmail.com>
2025-02-05 22:55:10 -0500

    Removed client from repo.

--- client/src/routes/dashboard/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-
-</script>
-
-<div class="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
-    <div class="max-w-md w-full space-y-8 p-8 rounded-lg shadow-md dark:bg-gray-800 dark:shadow-gray-700">
-
-    </div>
-</div>
